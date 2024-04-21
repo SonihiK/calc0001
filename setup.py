a669179579f4dbb3f18d25bb66abb397a1cb9ce5
SonihiK <162396305+SonihiK@users.noreply.github.com>
2024-04-21 22:21:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup (
     name = 'Sonihi_K',
     version='0.0.1',
-    description='nice calculator',
+    description='nice description',
     author = 'seongji',
     url = 'http://github.com/SonihiK/calc001',
     download_url = 'https://github.com/SonihiK/calc001/',
